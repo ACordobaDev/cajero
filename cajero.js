cb7ca90f8affd19a4180cea8732abb9a9e5181b6
Alejandro Cordoba <alejandrocordoba94@gmail.com>
2019-08-10 20:46:51 -0500

    el algoritmo esta funcionando sin problemas por ahora ( no ha sido facil - es mi primer proyecto con javascript)

--- cajero.js
+++ cajero.js
@@ -7,55 +7,76 @@ class Billete
         this.img = i;
     }
 }
+
+function Limpiar ()
+{resultado.innerHTML = "";}
+
 function EntregarDinero ()
 {
-
-     var t =document.getElementById("dinero");
+    var t =document.getElementById("dinero");
     dinero = parseInt( t.value);
+    Totalcaja= 0;
+    Salieron += dinero;
+
     for(var bi of caja)
     {
        if (dinero > 0 )
-       {   
-        Division = Math.floor(dinero / bi.valor);
-        if(Division > bi.cantidad)
-        {
-            Papeles = bi.cantidad;
-        }
-        else
-        {
-            Papeles = Division;
-        } 
-        entregado.push( new Billete(bi.valor, Papeles, bi.img) );
-        dinero = dinero - (bi.valor * Papeles);
-        bi.cantidad -= Papeles;
-          }
+       { 
+
+            Division = Math.floor(dinero / bi.valor);
+
+                if(Division > bi.cantidad)
+                {
+                    Papeles = bi.cantidad;
+                }
+
+                else
+                {
+                    Papeles = Division;
+                }
+            entregado.push( new Billete(bi.valor, Papeles, bi.img) );
+            dinero = dinero - (bi.valor * Papeles);
+            bi.cantidad -= Papeles;
+
+ 
+         }
+         totalencaja(bi.valor, bi.cantidad);
     }
+
     if(dinero > 0)
-    {
-       resultado.innerHTML = " No dinero";
-    }
+        {
+        resultado.innerHTML = " No dinero";
+        }
+    
     else 
     {
+       
         for (var e of entregado)
         { 
-            resultado.innerHTML = "";
+ 
+
             if (e.cantidad > 0)
-            {
-                resultado.innerHTML +=  e.cantidad + " Billetes de $" + e.valor + '<img src="'+e.img+'" height="42" width="42" />'+ "<br />" ;
-              
-               
-              }
-
-
-            }
+                {
+                    resultado.innerHTML +=  e.cantidad + " Billetes de $" + e.valor + '<img src="'+e.img+'" height="42" width="42" />'+ "<br />" + "Caja Total;"+ Totalcaja ;
+                }
+        }
+            console.log("Salieron", Salieron);
             
-            console.log(caja);
+            console.log( caja);
+            console.log("Dinero en caja", Totalcaja);
     }
 
 }
 
+function totalencaja (ValorBillete, cantidadBillete)
+{
+Totalcaja += (ValorBillete * cantidadBillete );
 
 
+}
+
+var Salieron = 0;
+var Totalcaja =0;
 var caja = [];
 var entregado = [];
 caja.push( new Billete (50, 3, "img/50.png" ) );
@@ -67,9 +88,8 @@ var Division = 0;
 var Papeles = 0;
 
 var resultado = document.getElementById("resultado");
-var resultado = document.getElementById("resultado");
-
-
-var resultado = document.getElementById("resultado");
 var BotonExtraer = document.getElementById("extraer");
 BotonExtraer.addEventListener("click", EntregarDinero);
+
+var limpiar = document.getElementById("limpiar");
+limpiar.addEventListener("click", Limpiar);
